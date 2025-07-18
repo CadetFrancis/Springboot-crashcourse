2b54c1d45377044d8b3b2a127b2a72d141f8658d
CadetFrancis <francis.belmes@gcash.com>
2025-07-18 21:08:38 +0800

    Added session2,lab10

---
+++ session2/lab10/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -0,0 +1,58 @@
+package com.example.demo.myfirstapi.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private Product product;
+
+    @BeforeEach
+    void setup() {
+        product = new Product(1L,"carbonara", 100d);
+        System.setOut(new PrintStream(outContent));
+    }
+    @Test
+    void validId(){
+        product.setId(3L);
+        assertEquals(3,product.getId());
+    }
+
+    @Test
+    void validName(){
+        product.setName("tyso");
+        assertEquals("tyso",product.getName());
+    }
+
+    @Test
+    void validPrice(){
+        product.setPrice(1500d);
+        assertEquals(1500d,product.getPrice());
+    }
+
+    @Test
+    void validReturnString(){
+        String expected = "Product{id=1, name='carbonara', price=100.0}";
+        assertEquals(expected,product.toString());
+    }
+    @Test
+    void validEquals(){
+        assertTrue(product.equals(product));
+    }
+    @Test
+    void validHashedObjects(){
+        Object product1 = new Product(1L, "carbonara", 100d);
+        Object product2 = new Product(1L,"carbonara", 100d);
+        String hashed1= String.valueOf(product1.hashCode());
+        String hashed2= String.valueOf(product2.hashCode());
+        assertEquals(hashed1, hashed2);
+    }
+
+}
---
+++ session2/lab10/src/test/java/com/example/demo/myfirstapi/repository/ProductRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.example.demo.myfirstapi.repository;
+
+import com.example.demo.myfirstapi.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+    private ProductRepository productRepository;
+    private Product product;
+    private final List<Product> products = Collections.synchronizedList(new ArrayList<>());
+    private final AtomicLong nextId = new AtomicLong(1);
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+
+    @BeforeEach
+    void setup(){
+        productRepository = new ProductRepository();
+        products.add(new Product(nextId.getAndIncrement(), "Laptop", 1200.00));
+        products.add(new Product(nextId.getAndIncrement(), "Mouse", 25.50));
+        products.add(new Product(nextId.getAndIncrement(), "Keyboard", 75.00));
+    }
+    @Test
+    void shouldFindAll(){
+        assertEquals(productRepository.findAll(),products);
+    }
+
+    @Test
+    void shouldFindById(){
+        Optional<Product> actualOptional = productRepository.findById(1L);
+        assertEquals(actualOptional,productRepository.findById(1L));
+    }
+
+    @Test
+    void shouldSaveNew(){
+        Product newProduct = new Product(null,"Watch",5000);
+        Product savedProduct = productRepository.save(newProduct);
+        assertEquals(4,savedProduct.getId());
+        assertEquals("Watch",savedProduct.getName());
+        assertEquals(5000,savedProduct.getPrice());
+    }
+
+    @Test
+    void shouldSaveExisting(){
+        Product newProduct = new Product(2L,"water",10);
+        Product existingProduct = productRepository.save(newProduct);
+        assertEquals(2,existingProduct.getId());
+        assertEquals("water",existingProduct.getName());
+        assertEquals(10d,existingProduct.getPrice());
+    }
+
+    @Test
+    void shouldThrowException(){
+        Product newProduct = new Product(50L,"Laptop",10);
+        assertEquals(newProduct,productRepository.save(newProduct));
+    }
+}
